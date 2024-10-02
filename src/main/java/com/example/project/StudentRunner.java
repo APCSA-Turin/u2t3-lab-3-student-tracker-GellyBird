ecf5049e9aaca496a6a17b3f52aa9eb9b5320eab
GellyBird <147560865+GellyBird@users.noreply.github.com>
2024-10-02 19:38:14 +0000

    first push

--- src/main/java/com/example/project/StudentRunner.java
+++ src/main/java/com/example/project/StudentRunner.java
@@ -1,5 +1,6 @@
 public class StudentRunner{
     public static void main(String[] args) {
         //use this StudentRunner class if you need to test your code 
+        Student s1 = new Student("india","jackson",2026)
     }
 }
